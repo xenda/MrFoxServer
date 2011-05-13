3fb4f7037d640ed8193d8818ae3c6d0a5a268375
Alvaro Pereyra <alvaro@xendacentral.com>
2011-05-13 13:22:24 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MrFoxServer::Application.config.secret_token = 'b8d42871c165625cdadb29979d7e5447e61e6d0ae2f1e284e624aa8ad11086d4cb81fc67d47d96d01a982c386e61d0bc19d27cafc83e678937d06108d00e5253'
